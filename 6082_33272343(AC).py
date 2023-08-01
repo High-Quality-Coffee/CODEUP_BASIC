8c2c48a668f55a2f4a2763b5ba1be350890626b8
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2023-08-02 07:34:52 +0900

    Add files via upload

---
+++ 6082_33272343(AC).py
@@ -0,0 +1,8 @@
+﻿n=int(input())
+for i in range(n):
+    if((i+1)>=10 and (i+1)%10!=0 and (i+1)%10%3==0):
+        print('X',end=" ")
+    elif((i+1)<10 and (i+1)%3==0 and (i+1)!=1):
+        print('X',end=" ")
+    else:
+        print(i+1,end=" ")
