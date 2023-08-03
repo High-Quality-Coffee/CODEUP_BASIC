843ae9cf3d9f5341c5bbf3e8d5ee0977e7e1a027
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2023-08-04 02:32:45 +0900

    Add files via upload

---
+++ 6083_33272346(AC).py
@@ -0,0 +1,9 @@
+﻿r,g,b=map(int, input().split())
+count=0
+
+for i in range(r):
+    for j in range(g):
+        for k in range(b):
+            print(i,j,k)
+            count=count+1
+print(count)
